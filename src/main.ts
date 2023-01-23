53786b8f32b350e92c9ea4c16065ebc5e6f75f91
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-01-23 16:51:24 +0100

    setting up env schema validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import cookieParser from 'cookie-parser'
 
 import { AppModule } from './modules/app.module'
+import express from 'express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  })
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+  })
+  app.useGlobalPipes(new ValidationPipe())
+  app.use(cookieParser)
+
+  //Setup to display files
+  app.use('/files', express.static('files'))
 
   const PORT = process.env.PORT || 8080
   await app.listen(PORT)
+
+  console.log(`App is listening on :${await app.getUrl()}`)
 }
 bootstrap()
