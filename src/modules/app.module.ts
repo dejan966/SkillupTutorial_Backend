53ed97ffc6b93727154e78f8b202a9141ba61e1a
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-01-24 12:33:09 +0100

    connected with database

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { configValidationSchema } from 'config/schema.config'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
