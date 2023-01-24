b7a6a5fc2848edf69bd472cc16946798bc28e364
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-01-24 17:00:37 +0100

    created CreateUserDto object

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common'
+import { User } from 'entities/user.entity'
+import { PaginatedResult } from 'interfaces/paginated-result.interface'
+import { UsersService } from './users.service'
+
+@Controller('users')
+@UseInterceptors(ClassSerializerInterceptor) //if you don't have this, everything from is user.entity.ts is excluded
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  async findAll(@Query('page') page: number): Promise<PaginatedResult> {
+    return this.usersService.paginate(page)
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  async findOne(@Param('id') id: string): Promise<User> {
+    return this.usersService.findById(id)
+  }
+
+  @Post(':id')
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto)
+  }
+}
---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@nestjs/common'
+import { AbstractService } from 'modules/common/abstract.service'
+
+@Injectable()
+export class UsersService extends AbstractService {}
