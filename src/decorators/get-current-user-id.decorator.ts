3b8502e7338c4372ef1eda3643934579209760eb
dejan966 <59951914+dejan966@users.noreply.github.com>
2023-01-25 10:24:56 +0100

    created routes for logging in, registering, signing out,..

--- src/decorators/get-current-user-id.decorator.ts
+++ src/decorators/get-current-user-id.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 import { User } from 'entities/user.entity'
 
-export const GetCurrentUser = createParamDecorator((_: undefined, context: ExecutionContext) => {
+export const GetCurrentUserId = createParamDecorator((_: undefined, context: ExecutionContext) => {
   const request = context.switchToHttp().getRequest()
   const user = request.user as User
   return user
